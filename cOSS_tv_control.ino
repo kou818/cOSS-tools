b84e28177a0567b0524f6b7f6dedf80fca5a53af
kou818 <kou818@gmail.com>
2016-06-26 23:51:10 +0800

    Add files via upload

---
+++ cOSS_tv_control.ino
@@ -0,0 +1,22 @@
+#include <IRremote.h>
+#include <IRremoteInt.h>
+
+/* Roger Kou          */
+/* kou818@gmail.com   */
+/* 2016-06-26         */
+
+IRsend irsend;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+ Serial.print("Starting\n");
+
+ irsend.sendSharp(0x68B92, 20);
+ delay(100);
+  
+}
+
